1db7e9efe5ee8395884740de488bc4281648e767
Morozov <Morozov@45b3efcf-09a2-e848-b922-4dba05e94977>
2007-03-01 15:04:00 +0000

    Реализация пожеланий Трэди по письму от Епихина за 16.02.

--- src/ReportSystem/CombToPlainReport.cs
+++ src/ReportSystem/CombToPlainReport.cs
@@ -48,20 +48,23 @@ select
   AllCoreT.note,
   AllCoreT.period,
   if(AllCoreT.junk, '1', '0'),
-  ActivePricesT.FirmName,
+  -- Вместо полного наименования выводим короткое наименование клиента
+  -- ActivePricesT.FirmName,
+  cd.ShortName,
   ActivePricesT.Region,
-  ActivePricesT.DateCurPrice,
-  replace( replace( replace(rd.OperativeInfo, '\t', ''), '\r', ''), '\n', '') as OperativeInfo,
+  -- ActivePricesT.DateCurPrice,
+  date_add(ActivePricesT.DateCurPrice, interval time_to_sec(date_sub(now(), interval unix_timestamp() second)) second) as DateCurPrice, 
+  -- replace( replace( replace(rd.OperativeInfo, '\t', ''), '\r', ''), '\n', '') as OperativeInfo,
   AllCoreT.Cost,
   AllCoreT.Quantity,
   cd.ShortName,
   rd.SupportPhone,
   cd.Fax as Fax,
   rd.adminmail,
-  cd.Url,
-  replace( replace( replace(rd.ContactInfo, '\t', ''), '\r', ''), '\n', '') as ContactInfo,
-  '' as MainManager,
-  cd.OrderManagerName
+  cd.Url -- ,
+  -- replace( replace( replace(rd.ContactInfo, '\t', ''), '\r', ''), '\n', '') as ContactInfo,
+  -- '' as MainManager,
+  -- cd.OrderManagerName
 INTO OUTFILE 'results/{0}'
 FIELDS TERMINATED BY '{1}'
 LINES TERMINATED BY '\n'
@@ -98,7 +101,7 @@ and cd.FirmCode = ActivePricesT.FirmCode
 				try
 				{
 					File.Copy(_sharePath + _filename, FileName, true);
-					//File.Delete(_sharePath + _filename);
+					File.Delete(_sharePath + _filename);
 					CopySucces = true;
 				}
 				catch (Exception e)
