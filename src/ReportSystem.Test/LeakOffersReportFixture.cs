330e9744b3e898344ecc9ecbae780cf4d46511e1
kvasov <kvasov@45b3efcf-09a2-e848-b922-4dba05e94977>
2010-04-15 08:41:20 +0000

    Файлы ошибочно не попавшие в коммит 354

---
+++ src/ReportSystem.Test/LeakOffersReportFixture.cs
@@ -0,0 +1,17 @@
+﻿using Inforoom.ReportSystem;
+using NUnit.Framework;
+
+namespace ReportSystem.Test
+{
+	[TestFixture]
+	public class LeakOffersReportFixture : BaseProfileFixture
+	{
+		[Test]
+		public void Make_report()
+		{
+			var props = TestHelper.LoadProperties(ReportsTypes.LeakOffers);
+			var report = new LeakOffersReport(0, "Automate Created Report", Conn, false, ReportFormats.Excel, props);
+			TestHelper.ProcessReport(report, ReportsTypes.LeakOffers);
+		}
+	}
+}
---
+++ src/ReportSystem/LeakOffersReport.cs
@@ -0,0 +1,80 @@
+using System.Data;
+using System.Linq;
+using Common.Tools;
+using ExecuteTemplate;
+using Inforoom.ReportSystem.Writers;
+using MySql.Data.MySqlClient;
+using DataTable = System.Data.DataTable;
+
+namespace Inforoom.ReportSystem
+{
+	public class LeakOffersReport : ProviderReport
+	{
+		public LeakOffersReport(ulong reportCode, string reportCaption, MySqlConnection connection, bool temporary, ReportFormats format, DataSet dsProperties)
+			: base(reportCode, reportCaption, connection, temporary, format, dsProperties)
+		{
+		}
+
+		public override void ReadReportParams()
+		{
+			_clientCode = (int)getReportParam("ClientCode");
+		}
+
+		public override void GenerateReport(ExecuteArgs e)
+		{
+			base.GenerateReport(e);
+			GetOffers(e);
+
+			e.DataAdapter.SelectCommand.CommandText = @"
+select c0.Code,
+c0.CodeCr,
+s.Synonym as Product,
+sfc.Synonym as Producer,
+c.Cost,
+c0.Quantity,
+c0.Period,
+c0.Note,
+c0.PriceCode
+from usersettings.core c
+join farm.core0 c0 on c0.Id = c.Id
+join farm.SynonymArchive s on s.SynonymCode = c0.SynonymCode
+join farm.SynonymFirmCr sfc on sfc.SynonymFirmCrCode = c0.SynonymFirmCrCode";
+			var data = new DataSet();
+			e.DataAdapter.Fill(data, "offers");
+
+			e.DataAdapter.SelectCommand.CommandText = @"
+select ap.PriceCode, cd.ShortName, ap.PriceName
+from usersettings.activeprices ap
+join usersettings.clientsdata cd on cd.FirmCode = ap.FirmCode";
+			e.DataAdapter.Fill(_dsReport, "prices");
+
+			var groupByPrice = data.Tables["offers"].Rows.Cast<DataRow>().GroupBy(r => r["PriceCode"]);
+
+			foreach (var price in groupByPrice)
+			{
+				var table = new DataTable(price.Key.ToString());
+				data.Tables["Offers"].Columns
+					.Cast<DataColumn>()
+					.Where(c => c.ColumnName != "PriceCode")
+					.Each(c => table.Columns.Add(c.ColumnName, c.DataType));
+				foreach (var offer in price)
+				{
+					var row = table.NewRow();
+					foreach (DataColumn column in table.Columns)
+						row[column.ColumnName] = offer[column.ColumnName];
+
+					table.Rows.Add(row);
+				}
+				_dsReport.Tables.Add(table);
+			}
+		}
+
+		protected override IWriter GetWriter(ReportFormats format)
+		{
+			if (format == ReportFormats.DBF)
+				throw new ReportException("Не умею готовить отчет в dbf");
+
+			return new LeakOffersWriter();
+		}
+	}
+}
