b0a69a4ddf48337ffc8bfe6215fb11e9405e9fa1
kvasov <kvasov@45b3efcf-09a2-e848-b922-4dba05e94977>
2010-05-13 14:11:55 +0000

    Исправил #2476. Корректировки в отчет: Предложения для аптеки

--- src/ReportSystem.Test/FastReports/FastReports.cs
+++ src/ReportSystem.Test/FastReports/FastReports.cs
@@ -1,4 +1,5 @@
-﻿using Inforoom.ReportSystem.Helpers;
+﻿using System.Data;
+using Inforoom.ReportSystem.Helpers;
 using NUnit.Framework;
 using Inforoom.ReportSystem.FastReports;
 using Inforoom.ReportSystem;
@@ -8,17 +9,69 @@ namespace ReportSystem.Test.FastReports
 	[TestFixture]
 	public class FastReports : BaseProfileFixture
 	{
-		[Test]
-		public void PharmacyOffersReportTest()
+		private DataSet properties;
+
+		[SetUp]
+		public void Setup()
 		{
 			ProfileHelper.Start();
+			properties = new DataSet();
+			var table = properties.Tables.Add("ReportProperties");
+			table.Columns.Add("PropertyName");
+			table.Columns.Add("PropertyValue");
+			table.Columns.Add("PropertyType");
+		}
 
-			var type = ReportsTypes.PharmacyOffers;
-			var props = TestHelper.LoadProperties(type);
-			var report = new PharmacyOffersReport(0, "Automate Created Report", Conn, false, ReportFormats.Excel, props);
-			TestHelper.ProcessReport(report, type);
-
+		[TearDown]
+		public void TearDown()
+		{
 			ProfileHelper.End();
+		}
+
+		[Test]
+		public void Build_report_with_producer()
+		{
+			AddProperty("IncludeQuantity", 1);
+			AddProperty("IncludeProducer", 1);
+			AddProperty("ClientCode", 2575);
+			BuildReport();
+		}
+
+		[Test]
+		public void Build_report_without_producer()
+		{
+			AddProperty("IncludeQuantity", 1);
+			AddProperty("IncludeProducer", 0);
+			AddProperty("ClientCode", 2575);
+			BuildReport();
+		}
+
+		[Test]
+		public void Build_report_with_cost_diff_threshold()
+		{
+			AddProperty("IncludeQuantity", 1);
+			AddProperty("IncludeProducer", 1);
+			AddProperty("CostDiffTheshold", 10);
+			AddProperty("ClientCode", 2575);
+			BuildReport();
+		}
+
+		public void AddProperty(string name, object value)
+		{
+			var row = properties.Tables[0].NewRow();
+			row["PropertyName"] = name;
+			row["PropertyValue"] = value;
+			if (value is int)
+				row["PropertyType"] = "INT";
+			else if (value is bool)
+				row["PropertyType"] = "BOOL";
+			properties.Tables[0].Rows.Add(row);
+		}
+
+		private void BuildReport()
+		{
+			var report = new PharmacyOffersReport(0, "Automate Created Report", Conn, false, ReportFormats.Excel, properties);
+			TestHelper.ProcessReport(report, ReportsTypes.PharmacyOffers);
 		}
 	}
 }
--- src/ReportSystem/FastReports/PharmacyOffersReport.cs
+++ src/ReportSystem/FastReports/PharmacyOffersReport.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using Inforoom.ReportSystem.Helpers;
 using MySql.Data.MySqlClient;
 
 namespace Inforoom.ReportSystem.FastReports
 {
+	public enum GroupOffersBy
+	{
+		Product,
+		ProductAndProducer
+	}
+
 	public class PharmacyOffersReport : BaseFastReport
 	{
 		private const string sql =
@@ -35,18 +42,25 @@ update Core cor set
 update Core cor set
   ProducerName = (select Name from catalogs.Producers where ID = cor.ProducerId);
 
-select  ProductId,
-        ProductName,
-        ProducerId,
-        ProducerName,
-        SupplierName,
-        min(cost) Cost
-  from Core c
-       join usersettings.PricesData pd on pd.PriceCode = c.PriceCode
-group by ProductId, ProducerId, pd.FirmCode
-order by ProductName, ProducerName, Cost;
+select  c.ProductId,
+        c.ProductName,
+        c.ProducerId,
+        c.ProducerName,
+        c.SupplierName,
+        min(c.cost) Cost,
+		c0.Quantity,
+		sfc.Synonym as Producer,
+		c0.Code
+from Core c
+	join farm.Core0 c0 on c0.Id = c.Id
+	left join farm.SynonymFirmCr sfc on sfc.SynonymFirmCrCode = c0.SynonymFirmCrCode
+	join usersettings.PricesData pd on pd.PriceCode = c.PriceCode
+group by c.ProductId, c.ProducerId, pd.FirmCode
+order by c.ProductName, c.ProducerName, Cost;
 ";
-		private int _clientId;
+		private bool _includeQuantity;
+		private bool _includeProducer;
+		private decimal _costDiffTheshold;
 		private int _suppliersCount = 0;
 
 		public PharmacyOffersReport(ulong ReportCode, string ReportCaption, MySqlConnection Conn, bool Temporary, ReportFormats format, DataSet dsProperties) 
@@ -56,13 +70,17 @@ order by ProductName, ProducerName, Cost;
 
 		public override void ReadReportParams()
 		{
-			_clientId = (int)getReportParam("ClientCode");
+			_clientCode = (int)getReportParam("ClientCode");
+			_includeProducer = Convert.ToBoolean(getReportParam("IncludeProducer"));
+			_includeQuantity = Convert.ToBoolean(getReportParam("IncludeQuantity"));
+			if (reportParamExists("CostDiffThreshold"))
+				_costDiffTheshold = Convert.ToDecimal(getReportParam("CostDiffThreshold"));
 		}
 
 		public override void GenerateReport(ExecuteTemplate.ExecuteArgs e)
 		{
 			ProfileHelper.Next("GetOffers");
-			GetOffers(e, _clientId);
+			GetOffers(e);
 
 			ProfileHelper.Next("GetData");
 			e.DataAdapter.SelectCommand.CommandText = sql;
@@ -82,55 +100,75 @@ order by ProductName, ProducerName, Cost;
 			var dataTable = new DataTable();
 			CustomizeResultTableColumns(dataTable);
 
-			string productName;
-			int productId;
+			decimal prevCost = 0;
+
 			int prevProductId = -1;
-			string producerName;
-			int producerId;
 			int prevProducerId = -1;
-			decimal cost = 0;
-			int supplierNumb = 0;
-			string supplierName;
+			int supplierIndex = 0;
 
 			DataRow row = null;
-			
 			while(reader.Read())
 			{
-				
-				productName = Convert.ToString(reader["ProductName"]);
-				productId = Convert.ToInt32(reader["ProductId"]);
-				producerName = Convert.ToString(reader["ProducerName"]);
-				producerId = (reader["ProducerId"] != DBNull.Value) ? Convert.ToInt32(reader["ProducerId"]) : -1;
-				supplierName = Convert.ToString(reader["SupplierName"]);
-				cost = Convert.ToDecimal(reader["Cost"]);
+				var productName = Convert.ToString(reader["ProductName"]);
+				var productId = Convert.ToInt32(reader["ProductId"]);
+				var producerName = Convert.ToString(reader["ProducerName"]);
+				var producerId = (reader["ProducerId"] != DBNull.Value) ? Convert.ToInt32(reader["ProducerId"]) : -1;
+				var supplierName = Convert.ToString(reader["SupplierName"]);
+				var cost = Convert.ToDecimal(reader["Cost"]);
 
-				if(productId != prevProductId ||
-					producerId != prevProducerId)
+				if(ShouldCreateNewRow(productId, prevProductId, producerId, prevProducerId))
 				{ // Стартуем новый Продукт
-					if (row != null)
-						dataTable.Rows.Add(row);
-
+					prevCost = 0;
+					AddRow(dataTable, row);
 					row = dataTable.NewRow();
 
+					row["Code"] = reader["Code"];
 					row["ProductName"] = productName;
-					row["ProducerName"] = producerName;
+					if (_includeProducer)
+						row["ProducerName"] = producerName;
 
-					supplierNumb = 0;
+					supplierIndex = 0;
 					prevProductId = productId;
 					prevProducerId = producerId;
 				}
 
-				supplierNumb++;
-				CheckSupplierNumb(supplierNumb, dataTable, row);
+				supplierIndex++;
+				CheckSupplierNumb(supplierIndex, dataTable, row);
 
-				row["Supplier" + supplierNumb] = supplierName;
-				row["Cost" + supplierNumb] = cost;
+				row["Supplier" + supplierIndex] = supplierName;
+				row["Cost" + supplierIndex] = cost;
+				row["Producer" + supplierIndex] = reader["Producer"].ToString();
+				if (_includeQuantity)
+					row["Quantity" + supplierIndex] = reader["Quantity"].ToString();
+				if (prevCost > 0)
+					row["Diff" + supplierIndex] = Math.Round(((cost - prevCost) / prevCost) *100, 2);
+				prevCost = cost;
 			}
 
-			if (row != null)
-				dataTable.Rows.Add(row);
+			AddRow(dataTable, row);
 
 			return dataTable;
+		}
+
+		private void AddRow(DataTable dataTable, DataRow row)
+		{
+			if (row == null)
+				return;
+
+			if (dataTable.Columns.Contains("Diff1")
+				&& row["Diff1"] != DBNull.Value
+				&& Convert.ToDecimal(row["Diff1"]) < _costDiffTheshold)
+				return;
+
+			dataTable.Rows.Add(row);
+		}
+
+		private bool ShouldCreateNewRow(int productId, int prevProductId, int producerId, int prevProducerId)
+		{
+			if (_includeProducer)
+				return productId != prevProductId || producerId != prevProducerId;
+			else
+				return productId != prevProductId;
 		}
 
 		private void CheckSupplierNumb(int numb, DataTable table, DataRow row)
@@ -150,28 +188,46 @@ order by ProductName, ProducerName, Cost;
 		{
 			_suppliersCount++;
 
-			DataColumn dc;
+			var dc = res.Columns.Add("Cost" + _suppliersCount, typeof(Decimal));
+			dc.Caption = "Цена";
+			dc.ExtendedProperties.Add("Width", (int?)6);
+
+			if (_includeQuantity)
+			{
+				dc = res.Columns.Add("Quantity" + _suppliersCount, typeof (string));
+				dc.Caption = "Остаток";
+				dc.ExtendedProperties.Add("Width", (int?) 4);
+			}
+
+			dc = res.Columns.Add("Producer" + _suppliersCount, typeof(string));
+			dc.Caption = "Производитель";
+			dc.ExtendedProperties.Add("Width", (int?)15);
+
+			dc = res.Columns.Add("Diff" + _suppliersCount, typeof(string));
+			dc.Caption = "Разница %";
+			dc.ExtendedProperties.Add("Width", (int?)5);
 
 			dc = res.Columns.Add("Supplier" + _suppliersCount, typeof(String));
 			dc.Caption = "Поставщик";
 			dc.ExtendedProperties.Add("Width", (int?)8);
-
-			dc = res.Columns.Add("Cost" + _suppliersCount, typeof(Decimal));
-			dc.Caption = "Цена";
-			dc.ExtendedProperties.Add("Width", (int?)6);
 		}
 
 		private void CustomizeResultTableColumns(DataTable res)
 		{
-			DataColumn dc;
+			var dc = res.Columns.Add("Code");
+			dc.Caption = "Код";
+			dc.ExtendedProperties.Add("Width", (int?) 6);
 
 			dc = res.Columns.Add("ProductName", typeof(String));
 			dc.Caption = "Наименование";
 			dc.ExtendedProperties.Add("Width", (int?) 15);
 
-			dc = res.Columns.Add("ProducerName", typeof(String));
-			dc.Caption = "Производитель";
-			dc.ExtendedProperties.Add("Width", (int?) 15);
+			if (_includeProducer)
+			{
+				dc = res.Columns.Add("ProducerName", typeof (String));
+				dc.Caption = "Производитель";
+				dc.ExtendedProperties.Add("Width", (int?) 15);
+			}
 		}
 	}
 }
