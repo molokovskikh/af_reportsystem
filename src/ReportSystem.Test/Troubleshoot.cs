e77f822c00869e495131a8d159cf4c90115bf979
kvasov <kvasov@45b3efcf-09a2-e848-b922-4dba05e94977>
2010-07-05 06:14:43 +0000

    Добавил возможность дебажить весь отчет

--- src/ReportSystem.Test/Troubleshoot.cs
+++ src/ReportSystem.Test/Troubleshoot.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
 using Inforoom.ReportSystem;
-using Inforoom.ReportSystem.FastReports;
 using MySql.Data.MySqlClient;
 using NUnit.Framework;
 
@@ -9,19 +11,93 @@ namespace ReportSystem.Test
 	[TestFixture, Ignore("Что бы подебажить отчет")]
 	public class Troubleshoot
 	{
+		string connectionString = "server=sql.analit.net;user=;password=; default command timeout=0;database=usersettings";
+
+		[SetUp]
+		public void Setup()
+		{
+			if (File.Exists("test.xls"))
+				File.Delete("test.xls");
+		}
+
 		[Test]
 		public void shoot_it()
 		{
-			uint reportcode = 1036;
-			using(var connection = new MySqlConnection("server=sql.analit.net;user=;password=; default command timeout=0;database=usersettings"))
+			uint reportcode = 501;
+			using(var connection = new MySqlConnection(connectionString))
 			{
 				connection.Open();
 				var loader = new ReportPropertiesLoader();
 				var prop = loader.LoadProperties(connection, reportcode);
-				var report = new PharmacyOffersReport(reportcode, "test", connection, true, ReportFormats.Excel, prop);
+/*				var row = prop.Tables[0].NewRow();
+				row["PropertyName"] = "StartDate";
+				row["PropertyValue"] = "2010-05-01";
+				row["PropertyType"] = "DATETIME";
+				prop.Tables[0].Rows.Add(row);
+				row = prop.Tables[0].NewRow();
+				row["PropertyName"] = "EndDate";
+				row["PropertyValue"] = "2010-06-01";
+				row["PropertyType"] = "DATETIME";
+				prop.Tables[0].Rows.Add(row);*/
+				var report = new SpecShortReport(reportcode, "test", connection, false, ReportFormats.Excel, prop);
 				report.ProcessReport();
 				report.ReportToFile(Path.GetFullPath("test.xls"));
 			}
 		}
+
+		[Test]
+		public void Troubleshoot_general_report()
+		{
+			uint id = 178;
+			var dataAdapter = new MySqlDataAdapter("",connectionString);
+			dataAdapter.SelectCommand.CommandText = @"
+select
+  * 
+from
+  reports.Reports r,
+  reports.reporttypes rt
+where
+    r.GeneralReportCode = ?reportcode
+and rt.ReportTypeCode = r.ReportTypeCode";
+				dataAdapter.SelectCommand.Parameters.AddWithValue("?reportcode", id);
+				var res = new DataTable();
+				dataAdapter.Fill(res);
+
+			using(var connection = new MySqlConnection(connectionString))
+			{
+				foreach (DataRow drGReport in res.Rows)
+				{
+					if (Convert.ToBoolean(drGReport[BaseReportColumns.colEnabled]))
+					{
+						var loader = new ReportPropertiesLoader();
+
+						//Создаем отчеты и добавляем их в список отчетов
+						var reportcode = (ulong) drGReport[BaseReportColumns.colReportCode];
+						Console.WriteLine("Отчет {0}", reportcode);
+						var prop = loader.LoadProperties(connection, reportcode);
+						var bs = (BaseReport) Activator.CreateInstance(
+							GetReportTypeByName(drGReport[BaseReportColumns.colReportClassName].ToString()),
+							new object[] {
+								reportcode,
+								drGReport[BaseReportColumns.colReportCaption].ToString(), connection,
+								false, ReportFormats.Excel,
+								prop
+							});
+						bs.ProcessReport();
+						bs.ReportToFile(Path.GetFullPath("test.xls"));
+
+					}
+				}
+			}
+		}
+
+		private Type GetReportTypeByName(string ReportTypeClassName)
+		{
+			Type t = Assembly.Load("ReportSystem").GetType(ReportTypeClassName);
+			if (t == null)
+				throw new ReportException(String.Format("Неизвестный тип отчета : {0}", ReportTypeClassName));
+			return t;
+		}
+
 	}
 }
