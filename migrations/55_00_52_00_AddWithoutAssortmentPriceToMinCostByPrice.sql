cf63688b084649203c3938bb9cf2b41a89ee0e45
Morozov Sergey <morozov@analit.net>
2011-02-25 14:51:36 +0300

    Новшество #3457 Отчет по мин.ценам - д.б. частным случаем отчета по мин.ценам по прайсу
    добавил новое свойство к отчету "Отчет по минимальным ценам по прайсу": "Рассчитать без учета ассортиментного прайс-листа".

---
+++ migrations/55_00_52_00_AddWithoutAssortmentPriceToMinCostByPrice.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO `reports`.`report_type_properties` 
+(`ReportTypeCode`, `PropertyName`, `DisplayName`, `PropertyType`, `Optional`, `DefaultValue`) 
+VALUES ('5', 'WithoutAssortmentPrice', 'Рассчитать без учета ассортиментного прайс-листа', 'BOOL', 0, '0');
