637a7c3888c66818c1a8e09d8b30d06e6c80a1fe
Тютин Алексей <a.tyutin@analit.net>
2011-05-23 11:47:44 +0400

    Добавил миграцию для добавлению опционального параметра UserCode

---
+++ migrations/142_00_add_optional_param_UserCode.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO `reports`.`report_type_properties` (`ReportTypeCode`, `PropertyName`, `DisplayName`, `PropertyType`, `Optional`, `DefaultValue`) VALUES (1, 'UserCode', 'Пользователь', 'ENUM', 1, '0');
