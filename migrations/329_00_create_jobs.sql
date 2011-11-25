6ac8f565ccf26e49eafff5617606fa459cf79070
Квасов Роман <r.kvasov@analit.net>
2011-11-25 17:17:31 +0400

    удалил лишнию миграцию

--- migrations/329_00_create_jobs.sql
+++
@@ -1,9 +0,0 @@
-
-    create table Reports.Jobs (
-        Id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-       Name VARCHAR(255),
-       NextRun DATETIME not null,
-       LastRun DATETIME,
-       RunInterval DATETIME not null,
-       primary key (Id)
-    );
