c9d419ceeb6626e81f7ec5652b4bea590e801937
Квасов Роман <r.kvasov@analit.net>
2011-11-28 14:53:45 +0400

    добавил протоколирование сохранения данных

--- src/Report.Data.Builder/CalculatorJob.cs
+++ src/Report.Data.Builder/CalculatorJob.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using Common.Tools.Calendar;
 using Common.Web.Ui.Models.Jobs;
+using log4net;
 
 namespace Report.Data.Builder
 {
 	public class CalculatorJob : IJob
 	{
+		private ILog log = LogManager.GetLogger(typeof (CalculatorJob));
 		public Config Config;
 		public DateTime Date = DateTime.Today.AddDays(-1);
 
@@ -21,7 +23,9 @@ namespace Report.Data.Builder
 			var costCalculator = new CostCalculator();
 			var offers = costCalculator.Offers(ratings, Config.ThreadCount);
 			var averageCosts = costCalculator.Calculate(offers);
+			log.DebugFormat("Начинаю сохранять средние цены");
 			costCalculator.Save(Date, averageCosts);
+			log.DebugFormat("Закончил сохранять средние цены");
 		}
 	}
 }
