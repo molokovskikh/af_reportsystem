043c813dd21a6d73498d4ef442ec6dc1f74a16f7
Квасов Роман <r.kvasov@analit.net>
2012-07-19 16:49:00 +0400

    добавил миграцию

---
+++ migrations/566_01_rating_report_do_not_show_absolute_values.sql
@@ -0,0 +1,14 @@
+insert into Reports.report_type_properties(ReportTypeCode,
+	PropertyName,
+	DisplayName,
+	PropertyType,
+	Optional,
+	DefaultValue)
+values (
+	(select ReportTypeCode from Reports.ReportTypes where ReportTypeFilePrefix = 'RatingReport'),
+	'DoNotShowAbsoluteValues',
+	'Скрывать все колонки кроме \'Доля рынка в %\' и \'Доля от общего заказа в %\'',
+	'BOOL',
+	0,
+	'0'
+)
