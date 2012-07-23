142a411548ce5e520542048c905d21fbd9dc43ff
Золотарев Александр <a.zolotarev@analit.net>
2012-07-23 11:29:05 +0400

    Написал тест и починил фикчу по требованию #10615 Настройка расписания отчетов - в определенные дни месяца

--- src/ReportTuner.Test/Functional/OneShortReportFixture.cs
+++ src/ReportTuner.Test/Functional/OneShortReportFixture.cs
@@ -7,66 +7,69 @@ using ReportTuner.Models;
 using WatiN.Core;
 using ReportTuner.Test.Helpers;
 using System.Diagnostics;
+using WatiN.Core.Native.Windows;
+using Test.Support.Web;
 
 namespace ReportTuner.Test.Functional
 {
 	[TestFixture]
-	class OneShortReportFixture : WatinFixture
+	public class OneShortReportFixture : WatinFixture2
 	{
 		[Test]
-		[Ignore]
 		public void TestOneShortReport()
 		{
 			using (var browser = new IE("http://localhost:53759/Reports/GeneralReports.aspx"))
 			{
-				browser.Visible = true;
 				var row = browser.Table(Find.ByClass("DocumentDataTable HighLightCurrentRow")).TableRows.First();
 				var row2 = (TableRow)row.NextSibling;
 				var cells = row2.OwnTableCells;
 				var cell = cells[0];
 				browser.GoTo("http://localhost:53759/Reports/schedule.aspx?r=" + cell.Text);
 
-				browser.Button(Find.ByValue("Выполнить")).Click();				
+				browser.Button(Find.ByValue("Выполнить")).Click();
 				Assert.That(browser.Text, Is.StringContaining("Успешно запущен разовый отчет"));
 
 				var processes = Process.GetProcesses();
 				var finded = false;
-				while (true)
-				{
-					Thread.Sleep(50);
-					if (processes.Any(process => process.ProcessName.Contains("ReportSystemBoot"))) {
+				for (int i = 0; i < 20; i++) {
+					if (processes.Any(process => process.ProcessName.Contains("ReportSystem"))) {
 						finded = true;
 					}
-					break;
+					Thread.Sleep(50);
 				}
 				Assert.That(finded, Is.True);
-				Thread.Sleep(15000);
-				browser.Refresh();
-				Thread.Sleep(15000);
-				Assert.That(browser.Text, Is.StringContaining("Операция выполнена"));
+			}
+		}
+
+		[Test]
+		public void Set_shedule_month()
+		{
+			using (var browser = new IE("http://localhost:53759/Reports/schedule.aspx?r=1")) {
+				browser.RadioButton(Find.ByValue("Ежемесячно")).Checked = true;
+				browser.Div("firstSixMonth").ChildOfType<CheckBox>(box => !box.Checked).Checked = true;
+				browser.Div("firstFifteenDays").ChildOfType<CheckBox>(box => !box.Checked).Checked = true;
+				browser.Button(Find.ByValue("Применить")).Click();
+				Assert.That(browser.Text, Is.StringContaining("Задать расписание для отчета "));
 			}
 		}
 
 		[Test]
 		public void Visit_every_report_type_configuration_page()
 		{
-			using(new SessionScope())
-			{
-				var types = ReportType.FindAll();
-				Assert.That(types.Length, Is.GreaterThan(0), "данные для тестов не загружены, выполни bake PrepareLocal profile=reports");
-				foreach (var type in types) {
-					var report = Report.Queryable.FirstOrDefault(r => r.ReportType == type);
+			var types = ReportType.FindAll();
+			Assert.That(types.Length, Is.GreaterThan(0), "данные для тестов не загружены, выполни bake PrepareLocal profile=reports");
+			foreach (var type in types) {
+				var report = Report.Queryable.FirstOrDefault(r => r.ReportType == type);
+				if (report != null)
 					CheckReport(report);
-				}
 			}
 		}
 
-		private static void CheckReport(Report report)
+		private void CheckReport(Report report)
 		{
 			var url = String.Format("http://localhost:53759/Reports/ReportProperties.aspx?rp={0}&r={1}", report.Id, report.GeneralReport.Id);
-			using (var browser = new IE(url)) {
-				Assert.That(browser.Text, Is.StringContaining("Настройка параметров отчета"));
-			}
+			browser = Open(url);
+			Assert.That(browser.Text, Is.StringContaining("Настройка параметров отчета"));
 		}
 	}
 }
