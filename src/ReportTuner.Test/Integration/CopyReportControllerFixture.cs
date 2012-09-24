209fcdb9a237dff68dfe9a2ab1e05d71f71d480a
Александр Шилин <a.shilin@analit.net>
2012-09-24 14:13:34 +0400

    Новшество #11803. Механизм переноса Листа (подотчета) в другой отчет. Добавлена логика и кнопка для копирования листа в выбранный отчет

---
+++ src/ReportTuner.Test/Integration/CopyReportControllerFixture.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.ActiveRecord;
+using Castle.MonoRail.TestSupport;
+using NHibernate;
+using NHibernate.Linq;
+using NUnit.Framework;
+using ReportTuner.Controllers;
+using ReportTuner.Models;
+
+namespace ReportTuner.Test.Integration
+{
+	public class CopyReportControllerFixture : BaseControllerTest
+	{
+		private ISession _session;
+
+		protected ISessionScope scope;
+
+		protected ISession session
+		{
+			get
+			{
+				if (_session == null) {
+					var holder = ActiveRecordMediator.GetSessionFactoryHolder();
+					_session = holder.CreateSession(typeof(ActiveRecordBase));
+				}
+				return _session;
+			}
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			scope = new SessionScope();
+		}
+
+		[Test]
+		public void CopyReportTest()
+		{
+			var generalReport1 = new GeneralReport {
+				Comment = "тестовый отчет1"
+			};
+			session.Save(generalReport1);
+			session.Flush();
+			var generalReport2 = new GeneralReport {
+				Comment = "тестовый отчет2"
+			};
+			session.Save(generalReport2);
+			session.Flush();
+			var report1 = new Report {
+				GeneralReport = generalReport1,
+				Enabled = true,
+				ReportCaption = "testReport1",
+				ReportType = session.Query<ReportType>().First(),
+			};
+			session.Save(report1);
+			session.Flush();
+			var property = session.Query<ReportProperty>().First(t => t.Report == report1);
+			property.Value = "propertyValue1";
+			session.Save(property);
+			session.Flush();
+			generalReport1.Reports.Add(report1);
+			session.Save(generalReport1);
+			session.Flush();
+			var controller = new CopyReportController();
+			PrepareController(controller);
+			controller.DbSession = session;
+			controller.CopyReport(generalReport2.Id, new GeneralReportsFilter {
+				Report = report1.Id,
+				GeneralReport = generalReport1.Id
+			});
+			session.Evict(generalReport2);
+			generalReport2 = session.Query<GeneralReport>().First(t => t.Id == generalReport2.Id);
+			var report2 = generalReport2.Reports.First();
+			Assert.That(report2, Is.Not.Null);
+			Assert.That(report2.Enabled, Is.EqualTo(report1.Enabled));
+			Assert.That(report2.ReportCaption, Is.EqualTo(String.Concat("Копия ", report1.ReportCaption)));
+			var properties2 = session.Query<ReportProperty>().Where(t => t.Report == report2);
+			var properties1 = session.Query<ReportProperty>().Where(t => t.Report == report1);
+			foreach (var reportProperty in properties1) {
+				Assert.That(properties2.Count(p => p.PropertyType == reportProperty.PropertyType && p.Value == reportProperty.Value), Is.Not.EqualTo(0));
+			}
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			if (_session != null) {
+				var holder = ActiveRecordMediator.GetSessionFactoryHolder();
+				holder.ReleaseSession(session);
+				_session = null;
+			}
+		}
+	}
+}
---
+++ src/ReportTuner/Controllers/CopyReportController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Castle.ActiveRecord;
+using Castle.MonoRail.Framework;
+using Common.MySql;
+using Common.Web.Ui.Controllers;
+using Common.Web.Ui.Helpers;
+using MySql.Data.MySqlClient;
+using NHibernate.Criterion;
+using NHibernate.Linq;
+using NHibernate.SqlCommand;
+using ReportTuner.Helpers;
+using ReportTuner.Models;
+using ViewHelper = ReportTuner.Helpers.ViewHelper;
+
+namespace ReportTuner.Controllers
+{
+	public class GeneralReportsFilter : PaginableSortable
+	{
+		public String ReportName { get; set; }
+		public UInt64 GeneralReport { get; set; }
+		public UInt64 Report { get; set; }
+
+
+		public GeneralReportsFilter()
+		{
+			SortKeyMap = new Dictionary<string, string> {
+				{ "Id", "Id" },
+				{ "Comment", "Comment" }
+			};
+		}
+		public IList<GeneralReport> Find()
+		{
+			var criteria = DetachedCriteria.For<GeneralReport>();
+			criteria.CreateCriteria("Payer", "p", JoinType.InnerJoin);
+			criteria.Add(Restrictions.Eq("Temporary", false));
+			criteria.Add(Restrictions.Like("Comment", String.Format("%{0}%", ReportName)));
+			var result = Find<GeneralReport>(criteria);
+			return result;
+		}
+	}
+
+	[Layout("MainLayout"),
+	Helper(typeof(ViewHelper)),
+	Helper(typeof(PaginatorHelper)),
+	Helper(typeof(ReportAppHelper), "app")]
+	public class CopyReportController : BaseController
+	{
+		public void SelectReport(ulong? rId, ulong? grId, [DataBind("filter")] GeneralReportsFilter filter)
+		{
+			PropertyBag["Reports"] = filter.Find();
+			PropertyBag["filter"] = filter;
+		}
+
+		public void CopyReport(ulong? destId, [DataBind("filter")] GeneralReportsFilter filter)
+		{
+			if(destId == null || filter.Report == null)
+				RedirectToUrl("../Reports/Reports.aspx?r=" + filter.GeneralReport);
+			var sourceReport = DbSession.Query<Report>().FirstOrDefault(r => r.Id == filter.Report);
+			if(sourceReport == null)
+				return;
+
+			var destReport = new Report {
+				Enabled = sourceReport.Enabled,
+				ReportCaption = String.Concat("Копия ", sourceReport.ReportCaption),
+				ReportType = sourceReport.ReportType,
+				GeneralReport = DbSession.Query<GeneralReport>().First(r => r.Id == destId)
+			};
+			using (new TransactionScope()) {
+				DbSession.Save(destReport);
+				DbSession.Flush();
+			}
+			ReportHelper.CopyReportProperties(sourceReport.Id, destReport.Id);
+			RedirectToUrl("../Reports/Reports.aspx?r=" + destId);
+		}
+	}
+}
