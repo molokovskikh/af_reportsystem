8d3a0f63deb9bb745d9c7f9b79f397a7598cacaa
Александр Шилин <a.shilin@analit.net>
2012-10-12 17:01:39 +0400

    Новшество #11803. Механизм переноса Листа (подотчета) в другой отчет. Исправлено падение отчета при поиске отчета на странице выбора целевого отчета для копирования - поля фильтра добавлены в hidden

---
+++ src/ReportTuner.Test/Functional/CopyReportFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Test.Support.Web;
+using WatiN.Core;
+
+namespace ReportTuner.Test.Functional
+{
+	[TestFixture]
+	public class CopyReportFixture : WatinFixture2
+	{
+		[Test]
+		public void SelectReportLinkTest()
+		{
+			Open("CopyReport/SelectReport?filter.Report=1&filter.GeneralReport=1&filter.ReportName=Тест");
+			browser.Link(Find.ByText("Тестовый отчет АК Инфорум")).Url.Contains("filter.GeneralReport=1&filter.Report=1&destId=1");
+			Assert.That(browser.Link(Find.ByText("Тестовый отчет АК Инфорум")).Url.Contains("filter.GeneralReport=1&filter.Report=1&destId=1"));
+			Click("Показать");
+			Assert.That(browser.Link(Find.ByText("Тестовый отчет АК Инфорум")).Url.Contains("filter.GeneralReport=1&filter.Report=1&destId=1"));
+		}
+	}
+}
