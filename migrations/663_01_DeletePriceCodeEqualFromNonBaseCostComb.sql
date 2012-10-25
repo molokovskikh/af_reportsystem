e5b6c178b436511d086840e708c76c7c775fc506
Александр Шилин <a.shilin@analit.net>
2012-10-25 11:01:10 +0400

    Новшество #8933. Настройка отчета Специальный. Добавлена миграция для удаления параметра список значений прайс для отчетов не по базовым ценам для комбинированного и дефектурного

---
+++ migrations/663_01_DeletePriceCodeEqualFromNonBaseCostComb.sql
@@ -0,0 +1,12 @@
+﻿drop temporary table IF EXISTS reports.TmpReportCode;
+CREATE temporary table reports.TmpReportCode(
+  ReportCode bigint unsigned
+  ) engine=MEMORY;
+  
+  insert into reports.TmpReportCode
+  SELECT reportcode FROM reports.report_properties r where propertyID in 
+  (select id from reports.report_type_properties rt where rt.reporttypecode=2 and propertyname='ByBaseCosts') and propertyvalue=0;
+  
+ delete from reports.report_properties where
+PropertyID in (select id from reports.report_type_properties rt where rt.reporttypecode=2 and rt.propertyname='PriceCodeEqual') 
+and reportcode in (select reportcode from reports.TmpReportCode);
