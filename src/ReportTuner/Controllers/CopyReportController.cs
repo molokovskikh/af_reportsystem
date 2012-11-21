b8fcf0bd3ceee274325f8315f0caff6dd208273e
Александр Шилин <a.shilin@analit.net>
2012-11-21 11:41:45 +0400

    Ошибка #12743. Медленно работает кнопка 'Скопировать в др.отчет'. Уменьшил размер страницы с отчетами до 15 записей, установил сортировку по коду по убыванию.

--- src/ReportTuner/Controllers/CopyReportController.cs
+++ src/ReportTuner/Controllers/CopyReportController.cs
@@ -30,6 +30,8 @@ namespace ReportTuner.Controllers
 				{ "Id", "Id" },
 				{ "Comment", "Comment" }
 			};
+			PageSize = 15;
+			SortDirection = "Desc";
 		}
 		public IList<GeneralReport> Find()
 		{
