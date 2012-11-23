1c9a669ab38f4f814dd8bb479f4cb7a362686532
Квасов Роман <r.kvasov@analit.net>
2012-11-23 15:35:24 +0400

    Новшество #13138 Выбор юр.лица в отчете для РосЗдрава

---
+++ migrations/693_00_alter_get_org_rollback.sql
@@ -0,0 +1,23 @@
+DROP PROCEDURE Reports.GetOrg;
+CREATE DEFINER=`RootDBMS`@`127.0.0.1` PROCEDURE Reports.`GetOrg`(in inFilter varchar(255), in inID int)
+begin
+  declare filterStr varchar(257);
+  if ((inFilter is not null) and (length(inFilter) > 0)) then
+    set filterStr = concat('%', inFilter, '%');
+    select
+      o.Id as ID,
+      concat(p.ShortName, ', ', ifnull(o.Name, ''))  as DisplayValue
+    from Billing.LegalEntities o
+		join Billing.Payers p on p.PayerId = o.PayerId
+    where
+      concat(p.ShortName, ', ', ifnull(o.Name, '')) like filterStr
+    order by concat(p.ShortName, ', ', ifnull(o.Name, ''));
+  else
+    select
+      o.Id as ID,
+      concat(p.ShortName, ', ', ifnull(o.Name, '')) as DisplayValue
+    from Billing.LegalEntities o
+		join Billing.Payers p on p.PayerId = o.PayerId
+    order by concat(p.ShortName, ', ', ifnull(o.Name, ''));
+  end if;
+end;
