e162f2b31cdf5c4becee5ea81079396ec60dec7b
Александр Шилин <a.shilin@analit.net>
2012-12-07 13:01:40 +0400

    Revert "Merge branch 'master' into i13307"
    This reverts commit 9bf639a9d18631944689597e279eab30e46e614d, reversing
    changes made to c9fc4d15d67c3d093399eb9811c71404651fe6df.

--- src/ReportSystem.Test/OffersReport/OffersReportFixture.cs
+++ src/ReportSystem.Test/OffersReport/OffersReportFixture.cs
@@ -7,10 +7,10 @@ using NUnit.Framework;
 
 namespace ReportSystem.Test
 {
-	[TestFixture]
+	[TestFixture, Ignore("Временно, выполняется слишком долго")]
 	public class OffersReportFixture : BaseProfileFixture
 	{
-		[Test, Ignore("Временно, выполняется слишком долго")]
+		[Test]
 		public void Offers_report_to_excel()
 		{
 			var props = TestHelper.LoadProperties(ReportsTypes.OffersReport);
@@ -18,103 +18,12 @@ namespace ReportSystem.Test
 			TestHelper.ProcessReport(report, ReportsTypes.OffersReport);
 		}
 
-		[Test, Ignore("Временно, выполняется слишком долго")]
+		[Test]
 		public void Offers_report_to_dbf()
 		{
 			var props = TestHelper.LoadProperties(ReportsTypes.OffersReport);
 			var report = new OffersReport(0, "Automate Created Report", Conn, ReportFormats.DBF, props);
 			TestHelper.ProcessReport(report, ReportsTypes.OffersReport);
-		}
-
-		[Test]
-		public void OffersReportByProducerCount()
-		{
-			var fileName = "OffersReportByProducerCount.xls";
-			Property("ReportType", 4);
-			Property("ClientCode", 5101);
-			Property("CalculateByCatalog", false);
-			Property("PriceCode", 216);
-			Property("ReportIsFull", false);
-			Property("MaxCostCount", 4);
-			report = new OffersReport(1, fileName, Conn, ReportFormats.Excel, properties);
-			BuildReport(fileName);
-		}
-
-		[Test]
-		public void OffersReportByProducer()
-		{
-			var fileName = "OffersReportByProducer.xls";
-			Property("ReportType", 3);
-			Property("ClientCode", 5101);
-			Property("CalculateByCatalog", false);
-			Property("PriceCode", 216);
-			Property("ReportIsFull", false);
-			Property("MaxCostCount", 4);
-			report = new OffersReport(1, fileName, Conn, ReportFormats.Excel, properties);
-			BuildReport(fileName);
-		}
-		[Test]
-		public void OffersReportByCount()
-		{
-			var fileName = "OffersReportByCount.xls";
-			Property("ReportType", 2);
-			Property("ClientCode", 5101);
-			Property("CalculateByCatalog", false);
-			Property("PriceCode", 216);
-			Property("ReportIsFull", false);
-			Property("MaxCostCount", 4);
-			report = new OffersReport(1, fileName, Conn, ReportFormats.Excel, properties);
-			BuildReport(fileName);
-		}
-
-		[Test]
-		public void OffersReport()
-		{
-			var fileName = "OffersReport.xls";
-			Property("ReportType", 1);
-			Property("ClientCode", 5101);
-			Property("CalculateByCatalog", false);
-			Property("PriceCode", 216);
-			Property("ReportIsFull", false);
-			Property("MaxCostCount", 4);
-			report = new OffersReport(1, fileName, Conn, ReportFormats.Excel, properties);
-			BuildReport(fileName);
-		}
-
-		[Test]
-		public void OffersReportByProducerCountBaseCost()
-		{
-			var fileName = "OffersReportByProducerCountBaseCost.xls";
-			Property("ReportType", 4);
-			Property("ClientCode", 5101);
-			Property("CalculateByCatalog", false);
-			Property("PriceCode", 216);
-			Property("ReportIsFull", false);
-			Property("MaxCostCount", 6);
-			Property("ByBaseCosts", true);
-			Property("RegionEqual", new List<ulong> {
-				4194304
-			});
-			report = new OffersReport(1, fileName, Conn, ReportFormats.Excel, properties);
-			BuildReport(fileName);
-		}
-
-		[Test]
-		public void OffersReportByProducerCountWeightCost()
-		{
-			var fileName = "OffersReportByProducerCountWeightCost.xls";
-			Property("ReportType", 4);
-			Property("ClientCode", 5101);
-			Property("CalculateByCatalog", false);
-			Property("PriceCode", 5699);
-			Property("ReportIsFull", false);
-			Property("MaxCostCount", 6);
-			Property("ByWeightCosts", true);
-			Property("RegionEqual", new List<ulong> {
-				1
-			});
-			report = new OffersReport(1, fileName, Conn, ReportFormats.Excel, properties);
-			BuildReport(fileName);
 		}
 	}
 }
