cbe4b1a046069bbb8c54df2ba7b661b6905bd847
Золотарев Александр <a.zolotarev@analit.net>
2013-03-13 11:20:42 +0400

    Выполнил #14441 Обновление генератора отчетов

--- src/ReportSystemBoot/Program.cs
+++ src/ReportSystemBoot/Program.cs
@@ -12,6 +12,8 @@ namespace ReportSystemBoot
 {
 	internal class Program
 	{
+		private const string AcceessKey = "/access:";
+
 		private static void DeployFiles(ILog logger)
 		{
 			try {
@@ -40,21 +42,50 @@ namespace ReportSystemBoot
 		{
 			XmlConfigurator.Configure();
 			var logger = LogManager.GetLogger(typeof(Program));
-
-			DeployFiles(logger);
+			var accessArgument = args.FirstOrDefault(a => a.StartsWith(AcceessKey));
+			var accessModified = accessArgument != null && Convert.ToBoolean(accessArgument.Substring(AcceessKey.Length));
+			if (!accessModified)
+				DeployFiles(logger);
 
 			var ass = Assembly.GetExecutingAssembly();
-			var bootAppName = Path.GetFileNameWithoutExtension(ass.Location).Replace("Boot", null);
+			var location = ass.Location;
+			var bootAppName = Path.GetFileNameWithoutExtension(location);
+			var appName = bootAppName;
+#if DEBUG
+			location = @"D:\Projects\ReportSystem\src\ReportSystem\bin\Debug";
+#else
+			location = Path.GetDirectoryName(ass.Location);
+#endif
 			var arg = args.Aggregate(string.Empty, (current, s) => current + " " + s);
 			if (args.Length >= 1)
 				bootAppName += arg;
 			logger.InfoFormat("Попытка запуска отчета: {0}", bootAppName);
 			try {
+				if (!accessModified) {
+					bootAppName += string.Format(" {0}true", AcceessKey);
 #if !DEBUG
-				ProcessStarter.StartProcessInteractivly(bootAppName, "runer", "zcxvcb", "analit");
+					ProcessStarter.StartProcessInteractivly(bootAppName, "runer", "zcxvcb", "analit");
 #else
-				ProcessStarter.StartProcessInteractivly(bootAppName, "tyutin", "*****", "analit");
+					ProcessStarter.StartProcessInteractivly(bootAppName, "Zolotarev", "*****", "analit");
 #endif
+				}
+				else {
+					AppDomain domain = null;
+					try {
+						var setup = new AppDomainSetup {
+							ApplicationBase = location,
+							ShadowCopyFiles = "true",
+							ShadowCopyDirectories = location,
+							ConfigurationFile = "ReportSystem.exe.config"
+						};
+						domain = AppDomain.CreateDomain("freeReportDomain", null, setup);
+						domain.ExecuteAssembly(Path.Combine(location, appName.Replace("Boot", ".exe")), args);
+					}
+					finally {
+						if (domain != null)
+							AppDomain.Unload(domain);
+					}
+				}
 				logger.InfoFormat("Отчет {0} отработал успешно", bootAppName);
 			}
 			catch (Exception exception) {
