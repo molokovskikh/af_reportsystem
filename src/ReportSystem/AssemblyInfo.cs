e70fc6fab7565a8711fe735a2af1a33d59bf939b
Квасов Роман <r.kvasov@analit.net>
2014-12-31 14:03:19 +0300

    Исправил сборку

--- src/ReportSystem/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("ReportSystem")]
-[assembly: AssemblyDescription("Программа для подготовки отчетов")]
-[assembly: AssemblyCompany("Inforoom")]
-[assembly: AssemblyProduct("ReportSystem")]
-[assembly: AssemblyCopyright("Copyright © Inforoom 2009")]
-[assembly: AssemblyVersion("2.2.35.61")]
